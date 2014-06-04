27705689eeda712ddd6e748952efa44e80b54c61
Prabhu <iamprabhu@yahoo.com>
2014-06-04 17:00:44 +0530

    Txn

--- SpringTXN/src/com/ds/service/AccountService.java
+++ SpringTXN/src/com/ds/service/AccountService.java
@@ -2,15 +2,21 @@ package com.ds.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.ds.dao.AccountDao;
 import com.ds.dao.StatementDao;
 
 @Service
 public class AccountService {
+	@Autowired
+	private TransactionTemplate transactionTemplate;
+	
 	@Autowired
 	private AccountDao accountDao;
 	@Autowired
@@ -27,8 +33,32 @@ public class AccountService {
 		statementDao.addStatement(accountNumber, type, amount);
 	}
 	public void withdraw(int accountNumber,String type, int amount) {
-		accountDao.withdraw(accountNumber, amount);
-		statementDao.addStatement(accountNumber, type, amount);
+		//Java 8
+/*		TransactionCallback<Object> txnCallback = (txnStatus)->{
+			try{
+				accountDao.withdraw(accountNumber, amount);
+				statementDao.addStatement(accountNumber, type, amount);
+			}
+			catch(Exception ex){
+				txnStatus.setRollbackOnly();
+			}
+			return null;
+		};
+		transactionTemplate.execute(txnCallback);
+*/
+	    //Java oldies comment this and try
+		transactionTemplate.execute(new TransactionCallback<Object>() {
+			public Object doInTransaction(TransactionStatus txnStatus) {
+				try{
+					accountDao.withdraw(accountNumber, amount);
+					statementDao.addStatement(accountNumber, type, amount);
+				}
+				catch(Exception ex){
+					txnStatus.setRollbackOnly();
+				}
+				return null;
+			}
+		});
 	}
 	@Transactional(propagation=Propagation.NOT_SUPPORTED)
 	public void deleteAllStatements(){
